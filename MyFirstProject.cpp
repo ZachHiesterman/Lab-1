2be38fc64a0ba8e372bff70299ce4c0099cec275
ZachHiesterman <zdhiesterman@gmail.com>
2024-09-04 08:35:31 -0500

    Add files via upload

---
+++ MyFirstProject.cpp
@@ -0,0 +1,18 @@
+/*
+	Zach Hiesterman
+	Computer Science Fall 2024
+	Due: September 3rd
+	Lab 1: Lab 1 introduction to C++ programming
+	This lab is a basic demo of C++ language.
+*/
+
+#include <iostream>		
+
+
+int main()
+{
+    std::cout << "Hello World!"<< std::endl; 
+    std::cout << "Red" << std::endl;
+
+	return 0;
+}
